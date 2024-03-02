3529200ea044e5d4a65915744ccc2e8142a2a12c
aaryaP777 <patilaarya106@gmail.com>
2024-03-02 20:53:27 +0530

    committed

---
+++ src/InvalidAgeExceptionHandler.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class InvalidAgeExceptionHandler {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Age : ");
+        int age = sc.nextInt();
+        try {
+            checkAgeHandler(age);
+        } catch (InvalidAgeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("oii bachelor kal aana");
+        }else {
+            System.out.println("Welcome sir welcome");
+        }
+    }
+
+    static void checkAgeHandler(int age) throws InvalidAgeException {
+        checkAge(age);
+    }
+
+}
+
+class InvalidAgeException extends Exception{
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+
+}
